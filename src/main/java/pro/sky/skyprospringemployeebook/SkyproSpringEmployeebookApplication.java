5e25ed269dd3982b7ec2b3c706c6127dc67c04af
Gamir07 <gamir07@yandex.ru>
2023-10-11 23:16:16 +0300

    init commit

---
+++ src/main/java/pro/sky/skyprospringemployeebook/SkyproSpringEmployeebookApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringemployeebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringEmployeebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringEmployeebookApplication.class, args);
+	}
+
+}
