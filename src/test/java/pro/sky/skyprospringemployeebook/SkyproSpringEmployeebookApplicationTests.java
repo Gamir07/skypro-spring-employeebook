24638b5c55936568925683326a32a7b337b6cf2b
Gamir07 <gamir07@yandex.ru>
2023-12-11 01:17:51 +0300

    Тесты забыл закинуть в гит

---
+++ src/test/java/pro/sky/skyprospringemployeebook/SkyproSpringEmployeebookApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringemployeebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringEmployeebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
